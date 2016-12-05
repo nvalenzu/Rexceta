204f2270b15ea9f0ff4b7771db0dbee9ab176606
JesiSalazar <jesi.salazar.n@gmail.com>
2016-12-05 00:09:44 -0300

    avance login facebook

--- app/src/main/java/cl/telematica/android/rexceta/LoginActivity.java
+++ app/src/main/java/cl/telematica/android/rexceta/LoginActivity.java
@@ -1,15 +1,22 @@
 package cl.telematica.android.rexceta;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -23,11 +30,29 @@ public class LoginActivity extends AppCompatActivity {
 
         LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
 
+
         LoginManager.getInstance().registerCallback(callbackManager,
                 new FacebookCallback<LoginResult>() {
                     @Override
                     public void onSuccess(LoginResult loginResult) {
-                        // App code
+                        GraphRequest request = GraphRequest.newMeRequest(
+                                loginResult.getAccessToken(),
+                                new GraphRequest.GraphJSONObjectCallback() {
+                                    @Override
+                                    public void onCompleted(JSONObject object, GraphResponse response) {
+                                        Log.v("LoginActivity", response.toString());
+                                        System.out.println(object.toString());
+
+                                        // Application code
+                                        try {
+                                            String mail = object.getString("email");
+                                            String birthday = object.getString("birthday");
+                                            System.out.println("HOLAAAA " + mail);
+                                        } catch (JSONException e) {
+                                            e.printStackTrace();
+                                        }
+                                    }
+                                });
                     }
 
                     @Override
@@ -41,9 +66,9 @@ public class LoginActivity extends AppCompatActivity {
                     }
                 });
     }
-   /* @Override
+    @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         callbackManager.onActivityResult(requestCode, resultCode, data);
-    }*/
+    }
 }

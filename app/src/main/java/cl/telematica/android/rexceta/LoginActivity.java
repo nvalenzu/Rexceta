c53045dbbff056b9a327fe82ac237923efcbc32c
JesiSalazar <jesi.salazar.n@gmail.com>
2016-12-05 22:53:43 -0300

    se conecta con cuenta de google devolviendo token valido

--- app/src/main/java/cl/telematica/android/rexceta/LoginActivity.java
+++ app/src/main/java/cl/telematica/android/rexceta/LoginActivity.java
@@ -2,8 +2,10 @@ package cl.telematica.android.rexceta;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
@@ -11,43 +13,65 @@ import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
 import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
+import com.facebook.Profile;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
 
     CallbackManager callbackManager = CallbackManager.Factory.create();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+
+        //FACEBOOK
         FacebookSdk.sdkInitialize(this.getApplicationContext());
         setContentView(R.layout.activity_login);
 
+
+        //GOOGLE
+        // Set the dimensions of the sign-in button.
+        SignInButton signInButton = (SignInButton) findViewById(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_STANDARD);
+        findViewById(R.id.sign_in_button).setOnClickListener(this);
+
+
+        //FACEBOOK
         LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions("public_profile");
+
+
 
 
         LoginManager.getInstance().registerCallback(callbackManager,
                 new FacebookCallback<LoginResult>() {
                     @Override
                     public void onSuccess(LoginResult loginResult) {
+                        Profile.getCurrentProfile();
+                        System.out.println("BLAAAAAAAAAAAAAAAAAAAH" + loginResult.getAccessToken().getToken());
                         GraphRequest request = GraphRequest.newMeRequest(
                                 loginResult.getAccessToken(),
                                 new GraphRequest.GraphJSONObjectCallback() {
                                     @Override
                                     public void onCompleted(JSONObject object, GraphResponse response) {
                                         Log.v("LoginActivity", response.toString());
-                                        System.out.println(object.toString());
+                                        System.out.println(object);
 
                                         // Application code
                                         try {
-                                            String mail = object.getString("email");
+                                            String firstName = Profile.getCurrentProfile().getFirstName();
                                             String birthday = object.getString("birthday");
-                                            System.out.println("HOLAAAA " + mail);
+                                            System.out.println("HOLAAAA " + firstName);
                                         } catch (JSONException e) {
                                             e.printStackTrace();
                                         }
@@ -57,7 +81,7 @@ public class LoginActivity extends AppCompatActivity {
 
                     @Override
                     public void onCancel() {
-                        // App code
+                        Log.v("LoginActivity", "cancel");
                     }
 
                     @Override
@@ -70,5 +94,17 @@ public class LoginActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i = new Intent(LoginActivity.this, IdTokenGoogleActivity.class);
+        startActivity(i);
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
